d293387b4b6c28c2d43b8df8f03feff33cc2f5f1
arinjayj1234 <80298655+arinjayj1234@users.noreply.github.com>
2022-08-11 18:42:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+stats = ''
+
+function setup() {
+    canvas = createCanvas(400, 280);
+    canvas.position(680, 400);
+
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function modelLoaded() {
+    console.log('Model Loaded');
+    stats = true;
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
