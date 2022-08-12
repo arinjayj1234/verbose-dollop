ebec81e1bfcb1d9c58d3c454a49251664c589adb
arinjayj1234 <80298655+arinjayj1234@users.noreply.github.com>
2022-08-11 18:45:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ stats = ''
 
 function setup() {
     canvas = createCanvas(400, 280);
-    canvas.position(710, 450);
+    canvas.position(750, 450);
 
     video = createCapture(VIDEO);
     video.size(380, 380);
